9fd232f5590bfcadb8063084c830d20ed7c06718
Gabor Nemet <gbr.nmt@gmail.com>
2014-01-22 09:30:13 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Demo2::Application.routes.draw do
   resources :microposts
   resources :users
 #  get "users/update"
-#  get "users/new"
+  get "users/new"
 #  get "static_pages/home"
 =begin
  get "static_pages/help"
@@ -12,7 +12,7 @@ Demo2::Application.routes.draw do
   root  'static_pages#home'
 
 
- # match '/signup',  to: 'users#new',            via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
